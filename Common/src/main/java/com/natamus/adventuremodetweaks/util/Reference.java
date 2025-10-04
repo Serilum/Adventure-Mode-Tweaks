4b6bcb32aa32c00563c1a6f8f848aa4595c27c91
Rick South <rick@natamus.com>
2025-10-04 19:37:08 +0200

    (v1.1) (1.21.9 Forge) Fixed incorrect remnant files included in the mod jar.

--- Common/src/main/java/com/natamus/adventuremodetweaks/util/Reference.java
+++ Common/src/main/java/com/natamus/adventuremodetweaks/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.adventuremodetweaks.util;
 public class Reference {
 	public static final String MOD_ID = "adventuremodetweaks";
 	public static final String NAME = "Adventure Mode Tweaks";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.21.9]";
 }
